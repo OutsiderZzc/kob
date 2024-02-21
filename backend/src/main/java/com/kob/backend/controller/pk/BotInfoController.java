7e81e75bda220673f691f104ce27ca271a9670d3
OutsiderZz <2924345841@qq.com>
2024-02-21 11:26:50 +0800

    day1

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,14 +3,19 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+import java.util.HashMap;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
 
     @RequestMapping("/getbotinfo/")
-    public String getBotInfo() {
-        String str = "Hello world!";
-        return str;
+    public Map<String, String> getBotInfo() {
+        Map<String, String> bot = new HashMap<>();
+        bot.put("name", "tiger");
+        bot.put("rating", "1500");
+        return bot;
     }
 
 }
